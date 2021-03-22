23c7cd1949f9aaf886a372f8eb87b24daecefe70
Jacklyn Biggin <jacklyn@jmb.im>
2021-03-22 19:41:39 -0400

    added ability to add facts

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { useState } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import Infocard from '../components/Infocard'
 import Winner from '../components/Winner'
@@ -6,20 +7,31 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
 
-  const dinoFacts = [
+  const [dinoFacts, setDinoFacts] = useState([
     ["🕹", "There is an amazing Chrome dino game"],
     ["👴", "They are very old, almost as old as Ryan Swift"],
-    ["7️⃣", "The old dinosaur ever found was at 7"],
     ["7️⃣", "The old dinosaur ever found was at 7"]
-  ]
+  ])
 
-  const melonFacts = [
+  const [melonFacts, setMelonFacts] = useState([
     ["☠️", "Jack is allergic to them"],
     ["🏆", "They have a Local Hack Day subguild thing named after them"],
-    ["🎉", "They are yeetful"],
     ["🎉", "They are yeetful"]
-  ]
+  ])
 
+  function addFact(type, fact, emoji) {
+    if(type == "melon"){
+      const tempFacts = [...melonFacts]
+      tempFacts.push([emoji, fact])
+      setMelonFacts(tempFacts)  
+    } else {
+      const tempFacts = [...dinoFacts]
+      tempFacts.push([emoji, fact])
+      setDinoFacts(tempFacts)  
+    }
+    return true
+  }
+  
   return (
     <>
       <header className={styles.header}>
@@ -48,7 +60,8 @@ export default function Home() {
           </Row>
         </Container>
         <Container className={styles.content}>
-          ...
+          <button onClick={() => {addFact("melon", "they suck", "💩")}}>set melons state</button>
+          <button onClick={() => {addFact("dino", "they suck", "💩")}}>set dinos state</button>
         </Container>
       </article>
     </>
